6b2a50ea9d46d9d80b3c7f4f59716bb244ab1def
Hermi Mohamed Rayen <mohamedrayenh501@gmail.com>
2025-10-18 13:49:55 +0100

    creation backend

---
+++ backend/src/test/java/com/iset/covtn/CovtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iset.covtn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
