3f86395e91b5d6862effdbf584080cb4579ac05c
mohamed amine maaroufi <mohamedaminecapo1224@gmail.com>
2025-10-19 11:42:28 +0100

    Authentification prete

---
+++ backend/src/main/java/models/Role.java
@@ -0,0 +1,7 @@
+package models;
+
+public enum Role {
+    ADMIN,
+    DRIVER,
+    PASSENGER
+}
---
+++ backend/src/main/java/models/UserInfo.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
+
+}
